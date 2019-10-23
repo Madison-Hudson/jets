f804577be3080f53cc31db3edae41ae8a3a26a91
Madison Hudson <owner_@Madisons-MacBook-Air.local>
2019-10-23 14:05:29 -0400

    add rockets

--- rocket.js
+++ rocket.js
@@ -1,10 +1,14 @@
-var Rocket = function (color, size) {
-    this.color = color;
-    this.size = size;
-  
-    this.launch = function () {
-      console.log('3, 2, 1, blastoff!');
-    }
-  }
-  
-  module.exports = Rocket;
+var Jet = function (color, size) {
+  this.color = color;
+  this.size = size;
+
+  this.fly = function () {
+    console.log('whoosh!');
+  };
+
+  this.takeOff = function () {
+    console.log('lift off!');
+  };
+}
+
+module.exports = Jet;
