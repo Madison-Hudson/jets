f482c33bcaa081cd4a3c4aa8eb66ab95b1e4134a
daveman235 <awad.david@gmail.com>
2019-10-23 14:41:43 -0400

    engines

--- rocket.js
+++ rocket.js
@@ -2,5 +2,8 @@ var Rocket = function (color, size) {
     this.color = color;
     this.size = size;
   }
+this.engageSecondaryEngines = function () {
+  console.log('Secondary Engines Engaged!');
+};
   
   module.exports = Rocket;
